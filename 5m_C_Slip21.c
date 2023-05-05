0e75c702acbf2ac9993cd83468e33a743d747ec6
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2023-05-05 11:27:19 +0530

    5m Slip 21

---
+++ 5m_C_Slip21.c
@@ -0,0 +1,13 @@
+// Slip - 21
+/*Write a program to calculate length of string using predefined function*/
+#include<stdio.h>
+#include<string>
+int main()
+{
+   char s1[100];
+    int k;
+   printf("Enter string:");
+   gets(s1);
+   k=strlen(s1);
+      printf("Length of string=%d",k);
+}
